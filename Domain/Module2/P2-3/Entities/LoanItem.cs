84d37af3bf0df349ba6977306d749dd77a9b1d07
sakicodes <officialkilan@gmail.com>
2026-03-23 03:18:32 +0800

    WIP: loan feature almost there

--- Domain/Module2/P2-3/Entities/LoanItem.cs
+++ Domain/Module2/P2-3/Entities/LoanItem.cs
@@ -2,5 +2,27 @@ namespace ProRental.Domain.Entities;
 
 public partial class Loanitem
 {
+	// Factory Method
+    public static Loanitem Create(int loanListId, int inventoryItemId, string? remarks = null)
+    {
+        var item = new Loanitem();
+        item.SetLoanListId(loanListId);
+        item.SetInventoryItemId(inventoryItemId);
+        item.SetRemarks(remarks);
+        return item;
+    }
+
+	// Business methods
+	public void UpdateRemarks(string? remarks) => SetRemarks(remarks);
+
+	// Getters
 	public int GetLoanItemId() => _loanitemid;
+	public int GetLoanListId() => _loanlistid;
+    public int GetInventoryItemId() => _inventoryitemid;
+    public string? GetRemarks() => _remarks;
+
+    // Setters
+    private void SetLoanListId(int id) => _loanlistid = id;
+    private void SetInventoryItemId(int id) => _inventoryitemid = id;
+    private void SetRemarks(string? remarks) => _remarks = remarks;
 }
