9297c2e804e6c1876eb8f978725786be3476ba32
MoeMaki33 <owenngo72@gmail.com>
2026-03-22 23:43:40 +0800

    Revert "Update"
    This reverts commit 5003e5b150fde1330daeaf313af8f1f3d459a91b.

--- Data/Module2/Interfaces/ISupplierSelectionStrategy.cs
+++
@@ -1,9 +0,0 @@
-using ProRental.Domain.Entities;
-
-namespace ProRental.Data.Interfaces
-{
-    public interface ISupplierSelectionStrategy
-    {
-        List<Supplier> SelectEligibleSuppliers(List<Supplier> suppliers, int stockId, int qty);
-    }
-}
